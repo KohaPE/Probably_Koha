9d28edfe6a4fb872dff169ab7331f7c42e8bdfa2
Koha <kohatx3@gmail.com>
2013-12-12 15:58:53 +1100

    Tweaks for resto

--- Shamen/resto.lua
+++ Shamen/resto.lua
@@ -16,23 +16,24 @@ ProbablyEngine.library.register('coreHealing', {
 
 ProbablyEngine.rotation.register_custom(264, "Koha's Resto Shamen", {
 
---Healing Rain
+--buffs
 { "Unleash Elements", { "modifier.shift", "@coreHealing.needsHealing(99, 1)" }, "lowest" },
+{ "Water Shield", "!player.buff" },
+
+--Healing Rain
+{ "Unleash Elements", { "modifier.shift", "lowest.health <= 100" }},
 { "Healing Rain", "modifier.shift", "ground" },
 
 --Interupts
-{ "Wind Shear", "modifier.interupt" },
+{ "Wind Shear", "modifier.interrupt" },
+{ "Spiritwalker's Grace", { "lowest.health < 50", "player.moving" }},
 
 --Totems
 { "Healing Stream Totem", { "@coreHealing.needsHealing(98, 2)" }, "lowest" },
 { "Healing Tide Totem", { "@coreHealing.needsHealing(60, 3)" }, "lowest" },
 { "Mana Tide Totem", "player.mana < 30" },
 { "Totemic Recall", { "player.glyph(41535).exists", "player.totem(Healing Stream Totem).duration <= 2", }},
-{ "Lightning Bolt", "player.mana < 15" },
-
-{{
-{ "Lightning Bolt", { "player.mana <= 100", "lowest.health >= 90" }},
-}, "toggle.dps" },
+{ "Lightning Bolt", { "lowest.health > 50", "player.mana < 15" }},
 
 --Cooldowns
 { "Spiritwalker's Grace", { "player.moving", "modifier.cooldowns", }},
@@ -46,14 +47,15 @@ ProbablyEngine.rotation.register_custom(264, "Koha's Resto Shamen", {
   { "Purify Spirit", "@coreHealing.needsDispelled('Lingering Corruption')" },
   { "Purify Spirit", { "player.buff(Power of the Titans)", "@coreHealing.needsDispelled('Mark of Arrogance')", }}, 
   { "Purify Spirit", "@coreHealing.needsDispelled('Corrosive Blood')" },
-
+--5.4 Healing
+  
 --Auto Target Enemy on DPS Toggle
 { "!/targetenemy [noharm]", { "toggle.dps", "!target.alive", "!target.enemy", "!target.exists", }},
 
 {{
 --Tank/Focus Healing
 { "Earth Shield", { "tank.exists", "!tank.buff(Earth Shield)", "!tank.buff(Water Sheild)", }, "tank" },
-{ "Riptide", { "!tank.buff", "tank.health <= 100" }},
+{ "Riptide", { "!tank.buff", "tank.health < 100" }},
 }, "!tank.range > 40" },
 
 {{
@@ -67,10 +69,13 @@ ProbablyEngine.rotation.register_custom(264, "Koha's Resto Shamen", {
 
 
 --Basic Healing
-{ "Riptide", { "lowest.health < 95", "!lowest.buff(Riptide)" }, "lowest" },
-{ "Healing Wave", "lowest.health < 86", "lowest" },
+{ "Riptide", { "lowest.health < 99", "!lowest.buff(Riptide)" }, "lowest" },
+{ "Healing Wave", "lowest.health < 90", "lowest" },
 }, "!lowest.range > 40" },
 
+{{
+{ "Lightning Bolt", "!target.range > 29" },
+}, "toggle.dps" },
 
 
 
@@ -82,7 +87,6 @@ ProbablyEngine.rotation.register_custom(264, "Koha's Resto Shamen", {
 { "Unleash Elements", { "modifier.shift", "@coreHealing.needsHealing(99, 1)" }, "lowest" },
 { "Healing Rain", "modifier.shift", "ground" },
 { "Greater Healing Wave", "@coreHealing.needsHealing(60, 1)", "lowest" },
-{ "Chain Heal", "@coreHealing.needsHealing(87, 3)", "lowest" },
 { "Healing Wave", "@coreHealing.needsHealing(93, 1)", "lowest" },
 { "Totemic Recall", { "player.glyph(41535).exists", "player.totem(Healing Stream Totem).duration <= 2", }},
 
