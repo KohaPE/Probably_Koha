8f971503a27be3e4b25667b10119f50444b72b65
Koha <kohatx3@gmail.com>
2013-12-30 19:23:43 +1100

    Druid Bear added cat form dps support tap control

--- Druid/bear.lua
+++ Druid/bear.lua
@@ -4,17 +4,50 @@
 -- Created on Nov 2nd 2013 1:03 am
 ProbablyEngine.rotation.register_custom(104, "DruidBearKoha", {
 
+{ "!/cast Bear Form", { "!player.buff(Bear Form)", "modifier.control" }},
+{ "!/cast Cat Form", { "!player.buff(Cat Form)", "modifier.control" }},
+{ "!/cast Bear Form", { "!player.buff(Bear Form)", "!player.buff(Cat Form)" }},
+
+--Cat Form DPS hold Control
+{{
+--AOE
+{{
+	{ "Ferocious Bite", "player.combopoints = 5" },
+	{ "Thrash", "!target.debuff(Thrash)" },
+	{ "Swipe" },
+}, "modifier.multitarget" },
+
+--Single Target
+{{
+	{ "Mangle", "!target.debuff(Infected Wounds)" },
+	{ "Ferocious Bite", { "player.combopoints = 5", "target.debuff(Rip).duration > 3" }},
+	{ "Rip", { "!target.debuff(Rip)", "player.combopoints = 5" }},
+	{ "Rake", "!target.debuff(Rake)" },
+	{ "Rake", "target.debuff(Rake).duration < 4" },
+	{ "Mangle", "player.combopoints < 5" }
+}, "!modifier.multitarget" },
+}, "player.buff(Cat Form)" },
 
 --Buffs
-	{ "Mark of the Wild", "!player.buff(Mark of the Wild)" },
-	{ "!/cast Bear Form", "!player.buff(Bear Form)" },
-	{ "Healing Touch", "player.buff(Dream of Cenarius)", "player" },
-
+{ "Mark of the Wild", { 
+	"!player.buff(Mark of the Wild)",
+	"!player.buff(Cat Form)", 
+    "!player.buff(Bear Form)", 
+    "!player.buff(Travel Form)", 
+    "!player.buff(Aquatic Form)", 
+    "!player.buff(Flight Form)", 
+    "!player.buff(Swift Flight Form)", 
+	"!player.buff(Treant Form)",
+}},
+{ "Healing Touch", "player.buff(Dream of Cenarius)", "player" },
+{{
 
 -- Interrupts
-  	{ "Skull Bash", "modifier.interrupts" },
-  	{ "Mighty Bash" , "modifier.interrupts" },
-	{ "Bear Hug", "modifier.interrupts" },
+{{
+  	{ "Skull Bash" },
+  	{ "Mighty Bash", { "player.spell(Skull Bash).cooldown", "!modifier.last(Skull Bash)" }},
+	{ "Bear Hug", { "player.spell(Skull Bash).cooldown", "!modifier.last(Skull Bash)", "!modifer.last(Mighty Bash)" }},
+}, "modifier.interrupts" },
 
 
 --Survival
@@ -41,13 +74,18 @@ ProbablyEngine.rotation.register_custom(104, "DruidBearKoha", {
     "modifier.cooldowns"
   	}},
   	{ "Natures Vigil" },
+	{ "Force of Nature", "!modifier.last(Forece of Nature)" },
 
 
 --AOE
-	{ "Thrash", "modifier.multitarget" },
-	{ "Swipe", "modifier.multitarget" },
+{{
+	{ "Thrash", "target.range <= 8" },
+	{ "Swipe", "target.range <= 8" },
+	{ "Mangle" },
+	}, "modifier.multitarget" },
 
 --Single Target
+{{
 	{ "Swipe", "modifier.alt" },
 	{ "Enrage", "player.rage < 20" },
 	{ "Mangle" },
@@ -56,6 +94,9 @@ ProbablyEngine.rotation.register_custom(104, "DruidBearKoha", {
 	{ "Maul", "player.buff(Tooth and Claw)" },
   	{ "Maul", "player.rage >= 85" },
 	{ "Faerie Fire" },
+	}, "!modifier.multitarget" },
+	
+}, "player.buff(Bear Form)" },
 	
 -- Out Of Combat
 },
